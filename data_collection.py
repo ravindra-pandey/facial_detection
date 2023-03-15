d2c514edae6a3d45740908fc1be5deae87f232a9
ravi-prog <ravindra3885@gmail.com>
2023-03-15 13:55:19 +0530

    data added

--- data_collection.py
+++ data_collection.py
@@ -1,23 +1,44 @@
+#########################################
+#  We will use this file if we want     #
+#    to collect the data                #
+#    using our own webcam               #
+# we will use thi code for getting empty#
+#########################################
+
+
+# Importing the libraries
 import os
 import time
 import cv2
 import uuid
 import random
 
+
+# Creating the necessary files in the project
 try:
-    os.mkdir("data")
-    for i in ['val','train','test',"images","labels"]:
-        os.mkdir(f"data/{i}")
-        for j in ["images","labels"]:
-            os.mkdir(f"data/{i}/{j}")
+    os.mkdir("facial_recognition/data")
+    for i in ['val', 'train', 'test', "images", "labels"]:
+        os.mkdir(f"facial_recognition/data/{i}")
+        for j in ["images", "labels"]:
+            if i not in ["images", "labels"]:
+                os.mkdir(f"facial_recognition/data/{i}/{j}")
 except Exception as e:
     print(e)
-    
-img_path=os.path.join("data","images")
-number_images=90
 
-cap=cv2.VideoCapture(0)
+# path of the data
+img_path = os.path.join("facial_recognition","data", "images")
 
-while True:
-    _,frame=cap.read()
-    
+number_images = 60
+
+cap = cv2.VideoCapture(0)
+
+for i in range(number_images):
+    _, frame = cap.read()
+    # writing the images
+    cv2.imwrite(os.path.join(img_path, f"{str(uuid.uuid1())}.jpg"), frame)
+    cv2.imshow("Collecting Data", frame)
+    time.sleep(0.5)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
